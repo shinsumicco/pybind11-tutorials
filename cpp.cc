a39eef6ce5200eddecd16d007c33551bc3dae661
shinsumicco <shinsumicco@gmail.com>
2018-06-13 17:47:12 +0900

    Add Python wappers

---
+++ cpp.cc
@@ -0,0 +1,18 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+#include <Eigen/Core>
+
+double summation(const Eigen::MatrixXd& X, const Eigen::MatrixXd& Y) {
+    double s = 0;
+    for (size_t i = 0; i < X.rows(); ++i) {
+        for (size_t j = 0; j < Y.rows(); ++j) {
+            s += std::log10((X(i) + Y(j)) * (X(i) + Y(j))) + std::sqrt(X(i) * Y(j));
+        }
+    }
+    return s;
+}
+
+PYBIND11_MODULE(cpp, m) {
+    m.def("summation", &summation);
+}
---
+++ openmp_cpp.cc
@@ -0,0 +1,29 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+#include <Eigen/Core>
+
+#include <iostream>
+
+double summation(const Eigen::MatrixXd& X, const Eigen::MatrixXd& Y) {
+#ifdef _OPENMP
+    std::cout << "(OpenMP is enabled.)" << std::endl;
+#else
+    std::cout << "(OpenMP is disabled.)" << std::endl;
+#endif
+    double s = 0;
+    size_t i, j;
+#ifdef _OPENMP
+#pragma omp parallel for private(j) reduction(+:s)
+#endif
+    for (i = 0; i < X.rows(); ++i) {
+        for (j = 0; j < Y.rows(); ++j) {
+            s += std::log10((X(i) + Y(j)) * (X(i) + Y(j))) + std::sqrt(X(i) * Y(j));
+        }
+    }
+    return s;
+}
+
+PYBIND11_MODULE(openmp_cpp, m) {
+    m.def("summation", &summation);
+}
